09562c58d3c8316082af72bf0f83e075f643cb45
AmirrezaDaneshvar <160042644+AmirrezaDaneshvar@users.noreply.github.com>
2024-02-15 18:39:36 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=0;i<6;i++){
+            if(i==3) {
+                continue;
+            }
+            System.out.println("**hello**");
+        }
+
+
+        }
+
+    }
